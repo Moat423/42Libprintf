4592ae6546733b39dcc81d89e56b37cd67169d9e
Lea Meubrink <lmeubrin@c3a1c1.42berlin.de>
2024-09-18 10:42:40 +0200

    added stringiteration to header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 10:02:32 by lmeubrin          #+#    #+#             */
-/*   Updated: 2024/07/02 14:46:42 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2024/09/16 14:17:38 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ int	ft_puthex_lower_fd(unsigned long nb, int fd, int *count);
 int	ft_puthex_upper_fd(unsigned int nb, int fd, int *count);
 int	ft_putchar_fd_return(char c, int fd);
 int	ft_putstr_fd_return(char *s, int fd);
+int	string_iteration(const char *str, va_list args);
 #endif
